4e00223d086a61cd6fe8dba1bafdf4e3cebb50d4
63070059 <73683000+63070059@users.noreply.github.com>
2022-04-09 21:00:38 +0700

    add book type2,3

--- Database/README_insert_type_1-3.sql
+++ Database/README_insert_type_1-3.sql
@@ -11,15 +11,58 @@ values
 
 insert into `author` (aut_pen_name, aut_fname, aut_lname)
 values
-('Uchida', 'Uchida', 'Yasuo'), 
-('Sako', 'Sako', 'Aizawa'),
-('Higashino', 'Higashino', 'Keigo'),
-('อิเกอิโดะ', 'อิเกอิโดะ', 'จุง'),
-('Ichikawa', 'Ichikawa', 'Yuto');
+('Uchida Yasuo', 'Uchida', 'Yasuo'), 
+('Sako Aizawa', 'Sako', 'Aizawa'),
+('Higashino Keigo', 'Higashino', 'Keigo'),
+('อิเกอิโดะ จุง', 'อิเกอิโดะ', 'จุง'),
+('Ichikawa Yuto', 'Ichikawa', 'Yuto');
 
 
 insert into `publisher` (pub_name, pub_phone, pub_url)
 values
-('Daifuku-Lit (ไดฟุกุ-ลีท)', '0024152621', 'https://www.facebook.com/DaifukuPUB/'),
-('เจคลาส', '0024266071', 'https://www.facebook.com/JClass-สำนักพิมพ์เจคลาส-1768815693405897'),
-('แพรวสำนักพิมพ์', '0123456789', 'https://www.facebook.com/PraewPublishing/');
+('Daifuku-Lit (ไดฟุกุ-ลีท)', '024152621', 'https://www.facebook.com/DaifukuPUB/'),
+('เจคลาส', '024266071', 'https://www.facebook.com/JClass-สำนักพิมพ์เจคลาส-1768815693405897'),
+('แพรวสำนักพิมพ์', null, 'https://www.facebook.com/PraewPublishing/');
+
+-- Type 2 --
+
+insert into `book` (book_title, book_price, book_amount, book_type, book_cover)
+values
+('คดีชุลมุนนักสืบเครื่องดูดฝุ่น', 385, 100, 2, '/images/คดีชุลมุนนักสืบเครื่องดูดฝุ่น.jpg'),
+('แออิน รักใหม่ หัวใจเดิม', 380, 80, 2, '/images/แออิน รักใหม่ หัวใจเดิม.jpg'),
+('วีรปริยา', 339, 70, 2, '/images/วีรปริยา.jpg');
+
+
+insert into `author` (aut_pen_name, aut_fname, aut_lname)
+values
+('Andra', null, null), 
+('ณารา', null, null),
+('Soeda Shin', 'Soeda', 'Shin');
+
+
+insert into `publisher` (pub_name, pub_phone, pub_url)
+values
+('Biblio', null, null);
+
+-- Type 3 --
+
+insert into `book` (book_title, book_price, book_amount, book_type, book_cover)
+values
+('การ์ตูน Dandadan เล่ม 2', 95, 69, 3, '/images/การ์ตูน Dandadan เล่ม 2.jpg'),
+('การ์ตูน Spy x Family เล่ม 1', 80, 20, 3, '/images/การ์ตูน Spy x Family เล่ม 1.jpg'),
+('การ์ตูน ไขคดีกับโคนันใน 10 นาที เล่ม 1', 115, 40, 3, '/images/การ์ตูน ไขคดีกับโคนันใน 10 นาที เล่ม 1.jpg'),
+('การ์ตูน One Piece เล่ม 99', 70, 200, 3, '/images/การ์ตูน One Piece เล่ม 99.jpg');
+
+
+insert into `author` (aut_pen_name, aut_fname, aut_lname)
+values
+('Yukinobu Tatsu', 'Yukinobu', 'Tatsu'), 
+('Tatsuya Endo', 'Tatsuya', 'Endo'),
+('Gosho Aoyama', 'Gosho', 'Aoyama'),
+('Eiichiro Oda', 'Eiichiro', 'Oda');
+
+
+insert into `publisher` (pub_name, pub_phone, pub_url)
+values
+('นานมีบุ๊คส์, บจก.', 02754319, null),
+('สยามอินเตอร์คอมิกส์, สนพ.', null, 'https://www.facebook.com/Siamintercomics/');
