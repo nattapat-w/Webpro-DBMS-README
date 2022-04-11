c36b6601b4a0d5e44d5b6588a10b323a2986a495
warit <63070157@kmitl.ac.th>
2022-04-11 19:46:29 +0700

    inserted into member

---
+++ Database/README_insert_member.sql
@@ -0,0 +1,11 @@
+insert into member (customer_cust_id,cust_mail)
+values
+        (1,'Apinya@hotmail.com'),
+        (2,'Apichart@gmail.com'),
+        (3,'Supartra@hotmail.com'),
+        (4,'Sirisa@hotmail.com'),
+        (5,'Sompart@hotmail.com'),
+        (6,'Varune@outlook.com'),
+        (7,'Rattapoom@hotmail.com'),
+        (8,'Parkpoum@gmail.com'),
+        (9,'Patmapron@gmail.com');
